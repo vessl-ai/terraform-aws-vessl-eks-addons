6ffa5c206432a785d045801ba66444bc54c3d8d3
Sean Lee <sean@vessl.ai>
2023-06-27 17:57:16 +0900

    First commit

---
+++ external_dns.tf
@@ -0,0 +1,42 @@
+locals {
+  domain_parts  = try(split(".", var.external_dns.cluster_domain), [])
+  domain_length = length(local.domain_parts)
+  domain_name   = try("${local.domain_parts[local.domain_length - 2]}.${local.domain_parts[local.domain_length - 1]}", "")
+}
+
+
+data "aws_route53_zone" "this" {
+  name = local.domain_name
+}
+
+resource "kubernetes_service" "tcp" {
+  count = var.external_dns != null && var.nginx_controller != null ? 1 : 0
+
+  metadata {
+    name      = "tcp"
+    namespace = var.external_dns.namespace
+    annotations = {
+      "external-dns.alpha.kubernetes.io/hostname"       = "tcp.${var.external_dns.cluster_domain}"
+      "external-dns.alpha.kubernetes.io/endpoints-type" = "NodeExternalIP"
+    }
+  }
+  spec {
+    cluster_ip = "None"
+    selector = {
+      "app.kubernetes.io/instance"  = "nginx-ingress-controller"
+      "app.kubernetes.io/component" = "controller"
+    }
+  }
+}
+
+module "aws_external_dns" {
+  count = var.external_dns != null ? 1 : 0
+
+  source                  = "./modules/aws-external-dns"
+  eks_cluster_name        = var.cluster_name
+  route53_hosted_zone_ids = [data.aws_route53_zone.this.id]
+  k8s_create_namespace    = false
+  k8s_namespace           = var.external_dns.namespace
+  helm_chart_version      = var.external_dns.version
+  tolerations             = var.tolerations
+}
