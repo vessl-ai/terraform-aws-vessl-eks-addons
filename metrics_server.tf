7921fbd460ac3a247bbd88c9773b4143754a2cb6
Sean Lee <sean@vessl.ai>
2024-03-15 19:51:30 +0900

    Refactor helm values

--- metrics_server.tf
+++ metrics_server.tf
@@ -1,24 +1,8 @@
 locals {
-  metrics_server_node_affinity = {
-    preferredDuringSchedulingIgnoredDuringExecution = [
-      for nodeSelector in var.node_selectors : {
-        weight = 1
-        preference = {
-          matchExpressions = [{
-            key      = nodeSelector.key
-            operator = "In"
-            values   = [nodeSelector.value]
-          }]
-        }
-      }
-    ]
-  }
-
-  // https://github.com/kubernetes-sigs/metrics-server/blob/796fc0f832c1ac444c44f88a952be87524456e07/charts/metrics-server/values.yaml
   metrics_server_helm_values = {
-    tolerations = var.tolerations
+    tolerations = local.tolerations
     affinity = {
-      nodeAffinity = local.metrics_server_node_affinity
+      nodeAffinity = local.node_affinity
     }
   }
 }
@@ -31,13 +15,5 @@ resource "helm_release" "metrics_server" {
   name       = "metrics-server"
   version    = var.metrics_server.version
   namespace  = var.metrics_server.namespace
-  values     = [yamlencode(local.metrics_server_helm_values)]
-
-  dynamic "set" {
-    for_each = var.metrics_server.helm_values
-    content {
-      name  = set.key
-      value = set.value
-    }
-  }
+  values     = [yamlencode(merge(local.metrics_server_helm_values, var.metrics_server.helm_values))]
 }
